d430ac7801296d9e4087a4a66597865cb3c1046a
Vagram <Vagramsaakyan@gmail.com>
2024-05-04 22:04:14 +0200

    skeleton updated

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ const PORT = process.env.PORT || 3001;
 
 const { Admin_Router } = require('./app/routes/admin/router.js');
 const { Client_Router } = require('./app/routes/client/router.js');
-const {DB_Router} = require('./app/Utils/db/router.js');
 
 // Admin router
 app.use('/admin', Admin_Router);
 
 // Client router
 app.use('/client', Client_Router);
-
-// Database router
-app.use('/DB', DB_Router);
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
