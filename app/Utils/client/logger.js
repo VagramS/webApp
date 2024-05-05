0a0c2300f9430b722315f937e62c893e868ff85d
Vagram <Vagramsaakyan@gmail.com>
2024-05-05 09:40:15 +0200

    logger added

---
+++ app/Utils/client/logger.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const winston = require('winston');
+//const Daily = require('winston-daily-rotate-file');
+
+const logDir = 'logs';
+
+// Create the log directory if it does not exist
+if(!fs.existsSync(logDir))
+    fs.mkdirSync(logDir);
+
+const myFormat = winston.format.printf(({ level, message, timestamp }) => {
+    return `${level}: ${message}, ${timestamp}  `;
+});
+
+const logger = winston.createLogger({
+    transports: [ new winston.transports.Console({
+            format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.colorize({all: true})), // colorize the output to the console
+            myFormat,
+        }),
+
+        new winston.transports.File({
+            filename: `${logDir}/error.log`,
+            level: 'error',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                myFormat
+            ),
+        }),
+
+        new winston.transports.File({
+            filename: `${logDir}/mixed.log`,
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                myFormat
+            ),
+        }),
+    ],
+});
+
+module.exports = logger;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require('./app/Utils/client/logger.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
